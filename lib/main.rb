a0a5720088250238178013bc6f49a975c03d7c08
hipale <hhemmhilb@gmail.com>
2022-09-23 20:35:14 +0200

    Add new player_won method

--- lib/main.rb
+++ lib/main.rb
@@ -8,8 +8,9 @@ end
 
 class Player
   def initialize
-    @hash = {}
-    @score = {}
+    $hash = {}
+    $score = {}
+    @picked_pos = []
   end
 
   def create_board(array, sign)
@@ -24,25 +25,45 @@ class Player
         end
         x += 1
       end
-      y += 1
-      x = 0
+      y += 1; x = 0
     end
   end
 
   def pick_a_number(num)
-     @score[num] == nil ? @score[num] = 0 : @score[num] += 1
-     @hash[num] = @score[num]
+     $score[num] == nil ? $score[num] = 0 : $score[num] += 1
+     $hash[num] = $score[num]
    
-    change_board(num - 1, @hash[num])
+    change_board(num - 1, $hash[num])
   end
 
   def change_board(num, highest_point)
-    puts num
-  picked_pos = [[num, highest_point]]
-  create_board(picked_pos, "O")
+  @picked_pos.push([num, highest_point])
+  create_board(@picked_pos, "[O]")
+  player_won(num, highest_point, "[O]")
+  end
+
+  def player_won(x, y, sign)
+    @dif_moves = [[x + 1, y], [x - 1, y], [x + 1, y + 1], [x + 1, y - 1], 
+    [x, y + 1], [x, y - 1], [x - 1, y + 1], [x - 1, y - 1]]
+          puts"C"
+          counter = 0
+          if @dif_moves.any? { |g| @picked_pos.any? { |elm| elm == g}}
+            until counter == 2
+              puts "D"
+              puts counter
+             g[0] += 1 if g[0] > x
+             g[0] -= 1 if g[0] < x
+             g[1] += 1 if g[0] > y
+             g[1] -= 1 if g[0] < y
+             counter += 1
+            end
+            return "Player Won"
+          end
   end
 end
 
 p = Player.new
-puts p.pick_a_number(5)
-puts p.pick_a_number(5)
+p.pick_a_number(5)
+p.pick_a_number(5)
+p.pick_a_number(5)
+p.pick_a_number(5)
