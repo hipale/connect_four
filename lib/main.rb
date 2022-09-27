0c9491e6131f4b8fcfcd5d2da6fc49589bf2d7b5
hipale <hhemmhilb@gmail.com>
2022-09-27 13:40:08 +0200

    Add line that checks if highest point is smaller than 7

--- lib/main.rb
+++ lib/main.rb
@@ -16,7 +16,6 @@ class Player
   end
 
   def turns(num)
-    
     pick_a_number(num)
     change_board(num - 1, $hash[num])
     new_board = create_board($picked_pos)
@@ -26,6 +25,7 @@ class Player
 
   def pick_a_number(num)
     $score[num] == nil ? $score[num] = 0 : $score[num] += 1
+    return if $score[num] > 6 
     $hash[num] = $score[num]
   end
 
@@ -58,34 +58,31 @@ class Player
     selected_pos = $picked_pos.sort.select { |elm| elm[2] == sign }
     game_end(sign) && return if num == 3
     return if selected_pos.length < 4
-    #print selected_pos[num]
     x = selected_pos[num][0]; y = selected_pos[num][1]
-   
     dif_moves = [
       [x + 1, y], [x - 1, y], [x + 1, y + 1], [x + 1, y - 1], 
       [x, y + 1], [x, y - 1], [x - 1, y + 1], [x - 1, y - 1]
     ] 
     if index != nil then dif_moves = [dif_moves[index]] end
-    print dif_moves
-    
     dif_moves.each_with_index do |elm, inx|
       if selected_pos[num + 1][0] == elm[0] && selected_pos[num + 1][1] == elm[1]
         inx = index if index != nil
         player_won(sign, num + 1, inx)
-        puts "as"
       end
     end
   end
 
   def game_end(sign)
-    puts "a"
     return "Player 1 Won" if sign == "[O]"
     return "Player 2 Won" if sign == "[X]"
   end
 end
 
-p = Player.new("[X]")
-c = Player.new("[O]")
+p = Player.new("[O]")
+p.turns(5)
+p.turns(5)
+p.turns(5)
+p.turns(5)
 p.turns(5)
 p.turns(5)
 p.turns(5)
